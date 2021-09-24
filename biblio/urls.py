616ed1444d47c50dfbd6ecda0d32ee216d2044cb
Daniel Santos <daniel.santos@octopass.com.br>
2021-09-24 12:44:53 -0300

    feat: create book detail page

--- biblio/urls.py
+++ biblio/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('livros/', views.BookListView.as_view(), name='livros'),
-    path('livros/<int:id>', views.BookDetailView.as_view(), name='detalhes')
+    path('livros/<int:pk>', views.BookDetailView.as_view(), name='detalhe-do-livro')
 ]
--- biblio/views.py
+++ biblio/views.py
@@ -32,22 +32,13 @@ def index(request):
 
 class BookListView(ListView):
     model = Book
-    # queryset = Book.objects.all()
+    queryset = Book.objects.all()
+    context_object_name = 'book_list'
     template_name = 'biblio/book-list.html'
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['book_list'] = Book.objects.all()
-        return context
 
 
 class BookDetailView(DetailView):
-    queryset = Book.objects.all()
-
-    def get_queryset(self, id): 
-        return Book.objects.filter(id=id)
+    model = Book
 
 
 class IndexView(TemplateView):
